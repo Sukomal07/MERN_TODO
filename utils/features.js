892c9d385cf8ddedf6996dea177b011de6663284
Sukomal07 <hypersukomal@gmail.com>
2023-04-05 12:34:48 +0530

    increase maxage

--- utils/features.js
+++ utils/features.js
@@ -4,7 +4,7 @@ export const sendCookie = (user , res , message , statusCode = 200) =>{
     const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
     res.status(statusCode)
     .cookie("token", token, {
-        maxAge: 900000, 
+        maxAge: 86400000, 
         httpOnly: true,
         sameSite:process.env.NODE_ENV === "Development" ? "lax": "none",
         secure:process.env.NODE_ENV === "Development" ? false: true
